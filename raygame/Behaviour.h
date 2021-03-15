7acc0d739220f344ad25ea12de5e01aac3f05de8
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-03-15 14:46:45 -0500

    Added Enabled to Behaviour

--- raygame/Behaviour.h
+++ raygame/Behaviour.h
@@ -6,5 +6,11 @@ class Behaviour
 {
 public:
 	virtual void update(Agent* agent, float deltaTime) = 0;
+
+	bool getEnabled() { return m_enabled; }
+	void setEnabled(bool enabled) { m_enabled = enabled; }
+
+private:
+	bool m_enabled = true;
 };
 
