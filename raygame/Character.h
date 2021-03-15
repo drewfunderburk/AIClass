01ad47c7402fd34151576fb171ca7190fd796615
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-03-15 14:48:00 -0500

    Increased Character default hp

--- raygame/Character.h
+++ raygame/Character.h
@@ -4,7 +4,7 @@ class Character : public Agent
 {
 public:
 	Character(float x, float y, float collisionRadius, const char* spriteFilePath, 
-		float health = 1, float damage = 1, float maxSpeed = 1, float maxForce = 1);
+		float health = 2, float damage = 1, float maxSpeed = 1, float maxForce = 1);
 
 	virtual float getHealth() { return m_health; }
 	virtual float takeDamage(float health) { return m_health -= health; }
