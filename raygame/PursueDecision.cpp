fa734c5f47ddb7f428cf456d35f283daaa5e2029
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-03-15 14:44:43 -0500

    Added Pursue Decision

---
+++ raygame/PursueDecision.cpp
@@ -0,0 +1,17 @@
+#include "PursueDecision.h"
+#include "ComplexEnemy.h"
+#include "WanderBehaviour.h"
+#include "PursueBehaviour.h"
+#include "EvadeBehaviour.h"
+
+void PursueDecision::makeDecision(Agent* agent, float deltaTime)
+{
+	ComplexEnemy* enemy = dynamic_cast<ComplexEnemy*>(agent);
+
+	if (!enemy)
+		return;
+
+	enemy->getPursue()->setEnabled(true);
+	enemy->getWander()->setEnabled(false);
+	enemy->getEvade()->setEnabled(false);
+}
